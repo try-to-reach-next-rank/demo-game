c0eab6b1b49b9233ff20a9149d78202090d2e65f
fatpandac8 <bachcodethings@gmail.com>
2025-11-13 15:13:43 +0700

    multithread in asset loading

--- src/main/java/com/example/demo/model/assets/AssetManager.java
+++ src/main/java/com/example/demo/model/assets/AssetManager.java
@@ -1,9 +1,11 @@
 package com.example.demo.model.assets;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.example.demo.engine.AssetLoader;
 import com.example.demo.utils.Animation;
@@ -16,10 +18,10 @@ import javafx.scene.text.Font;
 public class AssetManager {
     private static final AssetManager instance = new AssetManager();
 
-    private final Map<String, Animation> anims = new HashMap<>();
-    private final Map<String, Image> images = new HashMap<>();
-    private final Map<String, AudioClip> sounds = new HashMap<>();
-    private final Map<String, Media> musics = new HashMap<>();
+    private final Map<String, Animation> anims = new ConcurrentHashMap<>();
+    private final Map<String, Image> images = new ConcurrentHashMap<>();
+    private final Map<String, AudioClip> sounds = new ConcurrentHashMap<>();
+    private final Map<String, Media> musics = new ConcurrentHashMap<>();
     private final Map<String, Font> fonts = new ConcurrentHashMap<>();
 
     private final List<AssetLoader> loaders = List.of(
@@ -38,8 +40,39 @@ public class AssetManager {
 
 
     public void loadAll() {
-        for (AssetLoader loader : loaders) {
-            loader.loadInto(this);
+        /**
+         * Executor Service: provides a pool of treads and an API for assigning tasks to it
+         *                   It is a complete solution for asynchronous processing, managing in-memory queue
+         *                   and schedules submitted tasks based on thread availability
+         *
+         * newFixedThreadPool(): is a factory methods for Executor Service
+         */
+        ExecutorService executorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+                // init a thread pool of the size of the machine availableProcessors
+        );
+
+        /**
+         * Future: represent the result of an asynchronous operation -> check if operation is completed or not
+         */
+        try {
+            List<Callable<Void>> tasks = loaders.stream()
+                    .<Callable<Void>>map(loader -> () -> {
+                        loader.loadInto(this);
+                        return null;
+                    })
+                    .toList();
+
+            /**
+             * Execute the given tasks, returning a list of Future holding result and status
+             */
+            executorService.invokeAll(tasks);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        } finally {
+            executorService.shutdown();
         }
     }
 
